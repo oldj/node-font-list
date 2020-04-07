ca15ec970b5358fe6efd8d7ba5f6638f8744a819
oldj <oldj.wu@gmail.com>
2020-04-07 18:38:03 +0800

    In Windows, try to use PowerShell to get the font list first. #11

---
+++ libs/win32/getByPowerShell.js
@@ -0,0 +1,41 @@
+/**
+ * getByPowerShell
+ * @author: oldj
+ * @homepage: https://oldj.net
+ */
+
+const exec = require('child_process').exec
+
+const parse = (str) => {
+  let fonts = []
+  str.split('\n').map(ln => {
+    ln = ln.trim()
+    if (!ln || !ln.includes(':')) return
+
+    ln = ln.split(':')
+    if (ln.length !== 2 || ln[0].trim() !== 'Name') return
+
+    fonts.push(ln[1].trim())
+  })
+
+  return fonts
+}
+
+/*
+@see https://superuser.com/questions/760627/how-to-list-installed-font-families
+
+  [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+  (New-Object System.Drawing.Text.InstalledFontCollection).Families
+*/
+module.exports = () => new Promise((resolve, reject) => {
+  let cmd = `powershell -command "[System.Reflection.Assembly]::LoadWithPartialName(\\"System.Drawing\\");(New-Object System.Drawing.Text.InstalledFontCollection).Families"`
+
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      reject(err)
+      return
+    }
+
+    resolve(parse(stdout))
+  })
+})
--- libs/win32/index.js
+++ libs/win32/index.js
@@ -6,82 +6,22 @@
 
 'use strict'
 
-const os = require('os')
-const fs = require('fs')
-const path = require('path')
-const execFile = require('child_process').execFile
-const util = require('util')
-
-const p_copyFile = util.promisify(fs.copyFile)
-
-function tryToGetFonts (s) {
-  let a = s.split('\n')
-  if (a[0].includes('Microsoft')) {
-    a.splice(0, 3)
-  }
-
-  a = a.map(i => {
-    i = i
-      .split('\t')[0]
-      .split(path.sep)
-    i = i[i.length - 1]
-
-    if (!i.match(/^[\w\s]+$/)) {
-      i = ''
-    }
-
-    i = i
-      .replace(/^\s+|\s+$/g, '')
-      .replace(/(Regular|常规)$/i, '')
-      .replace(/^\s+|\s+$/g, '')
-
-    return i
-  })
-
-  return a.filter(i => i)
-}
-
-async function writeToTmpDir (fn) {
-  let tmp_fn = path.join(os.tmpdir(), 'node-font-list-fonts.vbs')
-  await p_copyFile(fn, tmp_fn)
-  return tmp_fn
-}
+const methods = [
+  require('./getByPowerShell'),
+  require('./getByVBS')
+]
 
 module.exports = async () => {
-  /*
-  or use Powershell to get the font list:
-    (New-Object System.Drawing.Text.InstalledFontCollection).Families
-  @see https://superuser.com/questions/760627/how-to-list-installed-font-families
-  */
+  let fonts = []
 
-  let fn = path.join(__dirname, 'fonts.vbs')
-
-  const is_in_asar = fn.includes('app.asar')
-  if (is_in_asar) {
-    fn = await writeToTmpDir(fn)
+  for (let method of methods) {
+    try {
+      fonts = await method()
+      break
+    } catch (e) {
+      console.log(e)
+    }
   }
 
-  return new Promise((resolve, reject) => {
-    let cmd = `cscript`
-
-    execFile(cmd, [fn], (err, stdout, stderr) => {
-      let fonts = []
-
-      if (err) {
-        reject(err)
-        return
-      }
-
-      if (stdout) {
-        //require('electron').dialog.showMessageBox({message: 'stdout: ' + stdout})
-        fonts = fonts.concat(tryToGetFonts(stdout))
-      }
-      if (stderr) {
-        //require('electron').dialog.showMessageBox({message: 'stderr: ' + stderr})
-        fonts = fonts.concat(tryToGetFonts(stderr))
-      }
-
-      resolve(fonts)
-    })
-  })
+  return fonts
 }
