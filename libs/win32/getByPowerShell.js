8f5ccaeaa754641ae82c7b2600b524853511642d
MoeHero <MoeHero@users.noreply.github.com>
2021-09-20 01:04:31 +0800

    Update getByPowerShell.js

--- libs/win32/getByPowerShell.js
+++ libs/win32/getByPowerShell.js
@@ -9,7 +9,7 @@ const exec = require('child_process').exec
 const parse = (str) => {
   let fonts = []
   str.split('\n').map(ln => fonts.push(ln.trim()))
-  return fonts
+  return fonts.filter(f => !!f && !f.includes(':'))
 }
 
 /*
@@ -19,7 +19,7 @@ const parse = (str) => {
   (New-Object System.Drawing.Text.InstalledFontCollection).Families
 */
 module.exports = () => new Promise((resolve, reject) => {
-  let cmd = `powershell -command "Add-Type -AssemblyName PresentationCore;$families=[Windows.Media.Fonts]::SystemFontFamilies;foreach($family in $families){$name='';if(!$family.FamilyNames.TryGetValue([Windows.Markup.XmlLanguage]::GetLanguage('zh-cn'),[ref]$name)){$name=$family.FamilyNames[[Windows.Markup.XmlLanguage]::GetLanguage('en-us')]}echo $name}"`
+  let cmd = `powershell -command "chcp 65001;Add-Type -AssemblyName PresentationCore;$families=[Windows.Media.Fonts]::SystemFontFamilies;foreach($family in $families){$name='';if(!$family.FamilyNames.TryGetValue([Windows.Markup.XmlLanguage]::GetLanguage('zh-cn'),[ref]$name)){$name=$family.FamilyNames[[Windows.Markup.XmlLanguage]::GetLanguage('en-us')]}echo $name}"`
 
   exec(cmd, { maxBuffer: 1024 * 1024 * 10 }, (err, stdout, stderr) => {
     if (err) {
