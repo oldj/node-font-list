4edbb1933b49a9bac1eedd63a31da16b487fe57d
oldj <oldj.wu@gmail.com>
2021-11-07 21:10:24 +0800

    Preferably use VBS to get fonts on Windows 7/8 and Powershell on Windows 10.

--- libs/win32/index.js
+++ libs/win32/index.js
@@ -6,13 +6,19 @@
 
 'use strict'
 
-const methods = [
-  require('./getByPowerShell'),
-  require('./getByVBS')
-]
+const os = require('os')
+const getByPowerShell = require('./getByPowerShell')
+const getByVBS = require('./getByVBS')
+
+const methods_new = [getByPowerShell, getByVBS]
+const methods_old = [getByVBS, getByPowerShell]
 
 module.exports = async () => {
   let fonts = []
+
+  // @see {@link https://stackoverflow.com/questions/42524606/how-to-get-windows-version-using-node-js}
+  let os_v = parseInt(os.release())
+  let methods = os_v >= 10 ? methods_new : methods_old
 
   for (let method of methods) {
     try {
